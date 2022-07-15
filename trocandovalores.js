e95bcffcf004f309b7e04ef69b9133ef44105779
stuvikes <100052365+stuvikes@users.noreply.github.com>
2022-07-15 01:21:28 -0300

    Add files via upload

---
+++ trocandovalores.js
@@ -0,0 +1,11 @@
+// preparando os valores de forma invertida
+let a = 'valor2';
+let b = 'valor1';
+
+// Trocando os valores de forma númerica
+let c = a;
+a = b;
+b = c;
+// Mostrandos os resultaods
+console.log(a);
+console.log(b);
